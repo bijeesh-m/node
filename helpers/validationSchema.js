23c1dbdbe3194ccdd07b03441de143ab0395a46e
bijeesh-m <bijeeshmankunnan@gmail.com>
2023-11-20 12:15:28 +0530

    fix

--- helpers/validationSchema.js
+++ helpers/validationSchema.js
@@ -1,9 +1,9 @@
-const joi = require('joi')
+const joi = require("joi");
 
 const authSchema = joi.object({
-    username:joi.string().lowercase().required(),
-    email:joi.string().email().lowercase().required(),
-    password:joi.string().min(2).required()
-})
+  username: joi.string().lowercase().required(),
+  email: joi.string().email().lowercase().required(),
+  password: joi.string().min(2).required(),
+});
 
-module.exports = {authSchema:authSchema}
+module.exports = { authSchema: authSchema };
